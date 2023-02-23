12dfe26dff16a3398553f27e662f3afb7f02cdee
glitchy <zschopper@gmail.com>
2023-02-23 18:04:06 +0100

    átnevezés 2

---
+++ src/myproject/Gyakorlok.java
@@ -0,0 +1,28 @@
+package Gyakorlok;
+
+public class MetodusokGyak1 {
+    public static void main(String[] args) {
+        int osszeg = elso10SzamOsszege();
+
+        kiir("Az első 10 szám összege: " + osszeg);
+        int a = 3, b = 7;
+        osszeg = osszead(a, b);
+        kiir("%d + %d = %d\n".format(a, b, osszeg));
+    }
+
+    private static int elso10SzamOsszege() {
+        int osszeg = 0;
+        for (int i = 0; i < 10; i++) {
+            osszeg += 10;
+        }
+        return osszeg;
+    }
+
+    private static int osszead(int a, int b) {
+        return a + b;
+    }
+
+    private static void kiir(String szoveg) {
+        System.out.println(szoveg);
+    }
+}
